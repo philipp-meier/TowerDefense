c23ed2d2bf307554fb89c3b587d6e81d21c1518a
Philipp Meier <philipp.meier@gmx.at>
2021-08-22 20:03:42 +0200

    Increased field. Updated styling.

--- scripts/services/AppService.ts
+++ scripts/services/AppService.ts
@@ -2,10 +2,10 @@ import { IAppConfig as IAppConfig } from "../classes/Interfaces.js";
 
 export const AppConfig: IAppConfig = {
 	appTitle: "Tower Defense",
-	fieldWidth: 800,
+	fieldWidth: 1200,
 	fieldHeight: 600,
 	rowCount: 6,
-	columnCount: 8
+	columnCount: 12
 };
 
 export class AppService {
