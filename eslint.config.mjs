8b3d21bb7a1abfd16edc31a7f7a8f9cc3c07d084
Philipp Meier <philipp@p-meier.dev>
2024-10-25 16:52:26 +0200

    chore: Updated dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.ts"]},
+  {ignores: ["**/node_modules/**", "dist/**"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
